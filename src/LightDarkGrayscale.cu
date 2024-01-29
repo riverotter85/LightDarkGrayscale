67ebe290369c92de617bad2a5504e60df7721647
Logan Davis <overhome2@gmail.com>
2024-01-29 16:18:47 -0500

    Fixed build errors

--- src/LightDarkGrayscale.cu
+++ src/LightDarkGrayscale.cu
@@ -4,6 +4,7 @@
 #include <filesystem>
 #include <list>
 #include <exception>
+#include <vector>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -12,36 +13,57 @@
 
 using namespace std;
 
-__global__ void bright(uchar *d_r, uchar *d_g, uchar *d_b, int percentage, uchar *d_bright) {
+// __global__ void bright(uchar *d_r, uchar *d_g, uchar *d_b, int percentage, uchar *d_bright) {
+//     int blockId = blockIdx.x + blockIdx.y * gridDim.x
+//                     + gridDim.x * gridDim.y * blockIdx.z;
+//     int threadId = blockId * (blockDim.x * blockDim.y * blockDim.z)
+//                             + (threadIdx.z * (blockDim.x * blockDim.y))
+//                             + (threadIdx.y * blockDim.x) + threadIdx.x;
+    
+//     float modifier = 1 + percentage * 0.01;
+//     d_bright[threadId] = (d_r[threadId] + d_g[threadId] + d_b[threadId]) * modifier;
+// }
+
+// __global__ void dark(uchar *d_r, uchar *d_g, uchar *d_b, int percentage, uchar *d_dark) {
+//     int blockId = blockIdx.x + blockIdx.y * gridDim.x
+//                     + gridDim.x * gridDim.y * blockIdx.z;
+//     int threadId = blockId * (blockDim.x * blockDim.y * blockDim.z)
+//                             + (threadIdx.z * (blockDim.x * blockDim.y))
+//                             + (threadIdx.y * blockDim.x) + threadIdx.x;
+    
+//     float modifier = 1 - percentage * 0.01;
+//     d_dark[threadId] = (d_r[threadId] + d_g[threadId] + d_b[threadId]) * modifier;
+// }
+
+// __global__ void grayscale(uchar *d_r, uchar *d_g, uchar *d_b, uchar *d_grayscale) {
+//     int blockId = blockIdx.x + blockIdx.y * gridDim.x
+//                     + gridDim.x * gridDim.y * blockIdx.z;
+//     int threadId = blockId * (blockDim.x * blockDim.y * blockDim.z)
+//                             + (threadIdx.z * (blockDim.x * blockDim.y))
+//                             + (threadIdx.y * blockDim.x) + threadIdx.x;
+    
+//     d_grayscale[threadId] = (d_r[threadId] + d_g[threadId] + d_b[threadId]) / 3;
+// }
+
+__global__ void applyLightDarkGrayscale(uchar *d_r, uchar *d_g, uchar *d_b, uchar *d_bright, uchar *d_dark, uchar *d_grayscale, int brightPercentage, int darkPercentage) {
     int blockId = blockIdx.x + blockIdx.y * gridDim.x
                     + gridDim.x * gridDim.y * blockIdx.z;
     int threadId = blockId * (blockDim.x * blockDim.y * blockDim.z)
                             + (threadIdx.z * (blockDim.x * blockDim.y))
                             + (threadIdx.y * blockDim.x) + threadIdx.x;
     
-    float modifier = 1 + percentage * 0.01;
+    // Bright
+    float modifier = 1 + brightPercentage * 0.01;
     d_bright[threadId] = (d_r[threadId] + d_g[threadId] + d_b[threadId]) * modifier;
-}
 
-__global__ void dark(uchar *d_r, uchar *d_g, uchar *d_b, int percentage, uchar *d_dark) {
-    int blockId = blockIdx.x + blockIdx.y * gridDim.x
-                    + gridDim.x * gridDim.y * blockIdx.z;
-    int threadId = blockId * (blockDim.x * blockDim.y * blockDim.z)
-                            + (threadIdx.z * (blockDim.x * blockDim.y))
-                            + (threadIdx.y * blockDim.x) + threadIdx.x;
-    
-    float modifier = 1 - percentage * 0.01;
+    // Dark
+    modifier = 1 - darkPercentage * 0.01;
     d_dark[threadId] = (d_r[threadId] + d_g[threadId] + d_b[threadId]) * modifier;
-}
 
-__global__ void grayscale(uchar *d_r, uchar *d_g, uchar *d_b, uchar *d_grayscale) {
-    int blockId = blockIdx.x + blockIdx.y * gridDim.x
-                    + gridDim.x * gridDim.y * blockIdx.z;
-    int threadId = blockId * (blockDim.x * blockDim.y * blockDim.z)
-                            + (threadIdx.z * (blockDim.x * blockDim.y))
-                            + (threadIdx.y * blockDim.x) + threadIdx.x;
-    
+    // Grayscale
     d_grayscale[threadId] = (d_r[threadId] + d_g[threadId] + d_b[threadId]) / 3;
+
+    __syncthreads();
 }
 
 __host__ void executeKernel(CudaImage *ci, int brightPercentage, int darkPercentage, int threadsPerBlock) {
@@ -52,27 +74,11 @@ __host__ void executeKernel(CudaImage *ci, int brightPercentage, int darkPercent
     dim3 grid(ci->rows, gridCols, 1);
     dim3 block(1, threadsPerBlock, blockZSize);
 
-    // Bright kernel
-    bright<<<grid, block>>>(ci->d_r, ci->d_g, ci->d_b, brightPercentage, ci->d_bright);
+    // Kernel code
+    applyLightDarkGrayscale<<<grid, block>>>(ci->d_r, ci->d_g, ci->d_b, ci->d_bright, ci->d_dark, ci->d_grayscale, brightPercentage, darkPercentage);
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess) {
         fprintf(stderr, "Failed to launch bright kernel: %s\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    // Dark kernel
-    dark<<<grid, block>>>(ci->d_r, ci->d_g, ci->d_b, darkPercentage, ci->d_dark);
-    err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        fprintf(stderr, "Failed to launch dark kernel: %s\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    // Grayscale kernel
-    grayscale<<<grid, block>>>(ci->d_r, ci->d_g, ci->d_b, ci->d_grayscale);
-    err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        fprintf(stderr, "Failed to launch grayscale kernel: %s\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
@@ -91,7 +97,8 @@ __host__ int promptInputPercentage(string prompt) {
 
 __host__ int main(int argc, char **argv) {
     const int threadsPerBlock = 8;
-    string filepaths;
+    vector<string> filepaths;
+    vector<CudaImage *> cudaImages;
 
     // Get a list of filenames
     string filepath = "./data/input";
@@ -107,24 +114,29 @@ __host__ int main(int argc, char **argv) {
         int darkPercentage = promptInputPercentage("Enter percentage to darken second image");
 
         // Iterate through each file
+        CudaImage *ci = NULL;
         for (string path : filepaths) {
-            CudaImage ci = createCudaImage(path);
+            ci = createCudaImage(path);
             copyFromHostToDevice(ci);
 
             // Execute kernel
             executeKernel(ci, brightPercentage, darkPercentage, threadsPerBlock);
+
+            cudaImages.push_back(ci);
         }
 
         // Synchronize all threads
-        __synchronize_threads();
+        // __syncthreads();
 
         // Now that our data operations are finished, commence with mapping to output files
-        for (string path : filepaths) {
-            copyFromDeviceToHost(ci);
+        for (int i = 0; i < filepaths.size(); ++i) {
+            copyFromDeviceToHost(cudaImages[i]);
 
-            mapBrightImage("bright_" + path);
-            mapDarkImage("dark_" + path);
-            mapGrayscaleImage("grayscale_" + path);
+            mapBrightImage(cudaImages[i], "bright_" + filepaths[i]);
+            mapDarkImage(cudaImages[i], "dark_" + filepaths[i]);
+            mapGrayscaleImage(cudaImages[i], "grayscale_" + filepaths[i]);
+
+            destroyCudaImage(cudaImages[i]);
         }
 
     } catch (Exception &e) {
