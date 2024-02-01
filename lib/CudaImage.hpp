befc6ded4acc5ebe9e8b6701b6b6075ab56709ad
Logan Davis <overhome2@gmail.com>
2024-02-01 17:42:26 -0500

    Added images for data; program is half-working

--- lib/CudaImage.hpp
+++ lib/CudaImage.hpp
@@ -5,6 +5,8 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 
+#include <cxxabi.h> # NOTE: Remove!!
+
 using namespace cv;
 using namespace std;
 
@@ -31,33 +33,37 @@ struct CudaImage {
 
 // Background functions
 
-tuple<int, int, uchar *, uchar *, uchar *> readImageFromFile(string inputFile) {
+__host__ tuple<int, int, uchar *, uchar *, uchar *> readImageFromFile(string inputFile) {
     Mat image = imread(inputFile, IMREAD_COLOR);
 
-    int rows = image.rows;
-    int cols = image.cols;
+    const int rows = image.rows;
+    const int cols = image.cols;
 
     uchar *h_r = (uchar *) malloc(sizeof(uchar) * rows * cols);
     uchar *h_g = (uchar *) malloc(sizeof(uchar) * rows * cols);
     uchar *h_b = (uchar *) malloc(sizeof(uchar) * rows * cols);
 
-    for (int row = 0; row < rows; ++row) {
-        for (int col = 0; col < cols; ++col) {
+    cout << "Before Hello\n";
+
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
             Vec3b intensity = image.at<Vec3b>(row, col);
             uchar b = intensity.val[0];
             uchar g = intensity.val[1];
             uchar r = intensity.val[2];
 
-            h_r[row * rows + col] = r;
-            h_g[row * rows + col] = g;
-            h_b[row * rows + col] = b;
+            h_r[row * cols + col] = r;
+            h_g[row * cols + col] = g;
+            h_b[row * cols + col] = b;
         }
     }
+
+    cout << "Hello\n";
 
     return {rows, cols, h_r, h_g, h_b};
 }
 
-uchar *allocateDeviceVector(int size) {
+__host__ uchar *allocateDeviceVector(size_t size) {
     uchar *vector = NULL;
     cudaError_t err = cudaMalloc(&vector, size);
     if (err != cudaSuccess) {
@@ -68,7 +74,7 @@ uchar *allocateDeviceVector(int size) {
     return vector;
 }
 
-void deallocateDeviceVector(uchar *vector) {
+__host__ void deallocateDeviceVector(uchar *vector) {
     cudaError_t err = cudaFree(vector);
     if (err != cudaSuccess) {
         fprintf(stderr, "Failed to free device vector: %s\n", cudaGetErrorString(err));
@@ -76,7 +82,7 @@ void deallocateDeviceVector(uchar *vector) {
     }
 }
 
-tuple<uchar *, uchar *, uchar *, uchar *, uchar *, uchar *> allocateDeviceMemory(int rows, int cols) {
+__host__ tuple<uchar *, uchar *, uchar *, uchar *, uchar *, uchar *> allocateDeviceMemory(int rows, int cols) {
     size_t size = rows * cols * sizeof(uchar);
 
     uchar *d_r         = allocateDeviceVector(size);
@@ -89,7 +95,7 @@ tuple<uchar *, uchar *, uchar *, uchar *, uchar *, uchar *> allocateDeviceMemory
     return {d_r, d_g, d_b, d_bright, d_dark, d_grayscale};
 }
 
-void deallocateMemory(uchar *d_r, uchar *d_g, uchar *d_b, uchar *d_bright, uchar *d_dark, uchar *d_grayscale) {
+__host__ void deallocateMemory(uchar *d_r, uchar *d_g, uchar *d_b, uchar *d_bright, uchar *d_dark, uchar *d_grayscale) {
     deallocateDeviceVector(d_r);
     deallocateDeviceVector(d_g);
     deallocateDeviceVector(d_b);
@@ -98,7 +104,7 @@ void deallocateMemory(uchar *d_r, uchar *d_g, uchar *d_b, uchar *d_bright, uchar
     deallocateDeviceVector(d_grayscale);
 }
 
-void cleanUpDevice() {
+__host__ void cleanUpDevice() {
     cudaError_t err = cudaDeviceReset();
     if (err != cudaSuccess) {
         fprintf(stderr, "Failed to reset the device state: %s\n", cudaGetErrorString(err));
@@ -106,7 +112,7 @@ void cleanUpDevice() {
     }
 }
 
-void copyHostDevice(uchar *src, uchar *dst, int size) {
+__host__ void copyHostDevice(uchar *src, uchar *dst, size_t size) {
     cudaError_t err = cudaMemcpy(dst, src, size, cudaMemcpyHostToDevice);
     if (err != cudaSuccess) {
         fprintf(stderr, "Failed to copy from host to device: %s\n", cudaGetErrorString(err));
@@ -114,7 +120,12 @@ void copyHostDevice(uchar *src, uchar *dst, int size) {
     }
 }
 
-void copyDeviceHost(uchar *src, uchar *dst, int size) {
+__host__ void copyDeviceHost(uchar *src, uchar *dst, size_t size) {
+    cout << "Hello!\n";
+    cout << "Src Type: " << abi::__cxa_demangle(typeid(src).name(), 0, 0, 0) << "\n";
+    cout << "Dst Type: " << abi::__cxa_demangle(typeid(dst).name(), 0, 0, 0) << "\n";
+    cout << "Size Type: " << abi::__cxa_demangle(typeid(size).name(), 0, 0, 0) << "\n";
+    cout << "Copy Type: " << abi::__cxa_demangle(typeid(cudaMemcpyDeviceToHost).name(), 0, 0, 0) << "\n";
     cudaError_t err = cudaMemcpy(dst, src, size, cudaMemcpyDeviceToHost);
     if (err != cudaSuccess) {
         fprintf(stderr, "Failed to copy from device to host: %s\n", cudaGetErrorString(err));
@@ -122,7 +133,7 @@ void copyDeviceHost(uchar *src, uchar *dst, int size) {
     }
 }
 
-void mapImage(uchar *filter, int rows, int cols, string outputFile) {
+__host__ void mapImage(uchar *filter, int rows, int cols, string outputFile) {
     Mat imageMat(rows, cols, CV_8UC1);
     vector<int> compressionParams;
     compressionParams.push_back(IMWRITE_PNG_COMPRESSION);
@@ -139,7 +150,7 @@ void mapImage(uchar *filter, int rows, int cols, string outputFile) {
 
 // Main controls
 
-CudaImage *createCudaImage(string inputImage) {
+__host__ CudaImage *createCudaImage(string inputImage) {
     auto[rows, cols, h_r, h_g, h_b] = readImageFromFile(inputImage);
     uchar *h_bright    = (uchar *) malloc(sizeof(uchar) * rows * cols);
     uchar *h_dark      = (uchar *) malloc(sizeof(uchar) * rows * cols);
@@ -147,11 +158,11 @@ CudaImage *createCudaImage(string inputImage) {
 
     tuple<uchar *, uchar *, uchar *, uchar *, uchar *, uchar *> deviceTuple = allocateDeviceMemory(rows, cols);
     uchar *d_r         = get<0>(deviceTuple);
-    uchar *d_g         = get<0>(deviceTuple);
-    uchar *d_b         = get<0>(deviceTuple);
-    uchar *d_bright    = get<0>(deviceTuple);
-    uchar *d_dark      = get<0>(deviceTuple);
-    uchar *d_grayscale = get<0>(deviceTuple);
+    uchar *d_g         = get<1>(deviceTuple);
+    uchar *d_b         = get<2>(deviceTuple);
+    uchar *d_bright    = get<3>(deviceTuple);
+    uchar *d_dark      = get<4>(deviceTuple);
+    uchar *d_grayscale = get<5>(deviceTuple);
 
     // Set properties in new CudaImage project
     CudaImage *cudaImage = (CudaImage *) malloc(sizeof(CudaImage));
@@ -178,13 +189,17 @@ CudaImage *createCudaImage(string inputImage) {
     return cudaImage;
 }
 
-void destroyCudaImage(CudaImage *cudaImage) {
+__host__ void toString(CudaImage *cudaImage) {
+    
+}
+
+__host__ void destroyCudaImage(CudaImage *cudaImage) {
     deallocateMemory(cudaImage->d_r, cudaImage->d_g, cudaImage->d_b,
                     cudaImage->d_bright, cudaImage->d_dark, cudaImage->d_grayscale);
     cleanUpDevice();
 }
 
-void copyFromHostToDevice(CudaImage *cudaImage) {
+__host__ void copyFromHostToDevice(CudaImage *cudaImage) {
     size_t size = cudaImage->rows * cudaImage->cols * sizeof(uchar);
 
     copyHostDevice(cudaImage->h_r, cudaImage->d_r, size);
@@ -192,23 +207,29 @@ void copyFromHostToDevice(CudaImage *cudaImage) {
     copyHostDevice(cudaImage->h_b, cudaImage->d_b, size);
 }
 
-void copyFromDeviceToHost(CudaImage *cudaImage) {
+__host__ void copyFromDeviceToHost(CudaImage *cudaImage) {
     size_t size = cudaImage->rows * cudaImage->cols * sizeof(uchar);
 
-    copyDeviceHost(cudaImage->h_r, cudaImage->d_r, size);
-    copyDeviceHost(cudaImage->h_g, cudaImage->d_g, size);
-    copyDeviceHost(cudaImage->h_b, cudaImage->d_b, size);
+    printf("D: %p\n", cudaImage->d_r);
+    printf("D: %p\n", cudaImage->d_g);
+    printf("D: %p\n", cudaImage->d_b);
+    printf("H: %p\n", cudaImage->h_r);
+    printf("H: %p\n", cudaImage->h_g);
+    printf("H: %p\n", cudaImage->h_b);
+    copyDeviceHost(cudaImage->d_r, cudaImage->h_r, size);
+    copyDeviceHost(cudaImage->d_g, cudaImage->h_g, size);
+    copyDeviceHost(cudaImage->d_b, cudaImage->h_b, size);
 }
 
-void mapBrightImage(CudaImage *cudaImage, string outputFile) {
+__host__ void mapBrightImage(CudaImage *cudaImage, string outputFile) {
     mapImage(cudaImage->h_bright, cudaImage->rows, cudaImage->cols, outputFile);
 }
 
-void mapDarkImage(CudaImage *cudaImage, string outputFile) {
+__host__ void mapDarkImage(CudaImage *cudaImage, string outputFile) {
     mapImage(cudaImage->h_dark, cudaImage->rows, cudaImage->cols, outputFile);
 }
 
-void mapGrayscaleImage(CudaImage *cudaImage, string outputFile) {
+__host__ void mapGrayscaleImage(CudaImage *cudaImage, string outputFile) {
     mapImage(cudaImage->h_grayscale, cudaImage->rows, cudaImage->cols, outputFile);
 }
 
--- src/LightDarkGrayscale.cu
+++ src/LightDarkGrayscale.cu
@@ -31,19 +31,21 @@ __global__ void applyLightDarkGrayscale(uchar *d_r, uchar *d_g, uchar *d_b, ucha
     // Grayscale
     d_grayscale[threadId] = (d_r[threadId] + d_g[threadId] + d_b[threadId]) / 3;
 
-    __syncthreads();
+    // __syncthreads();
 }
 
-__host__ void executeKernel(CudaImage *ci, int brightPercentage, int darkPercentage, int threadsPerBlock) {
+__host__ void executeKernel(CudaImage *ci, int brightPercentage, int darkPercentage, dim3 threadsPerBlock, dim3 blocksPerGrid) {
     cout << "Running kernel for LightDarkGrayscale...\n";
 
-    const int blockZSize = 4;
-    const int gridCols = min(ci->cols / (threadsPerBlock * 4), 1);
-    dim3 grid(ci->rows, gridCols, 1);
-    dim3 block(1, threadsPerBlock, blockZSize);
+    // const int blockZSize = 4;
+    // const int gridCols = min(ci->cols / (threadsPerBlock * 4), 1);
+    // dim3 grid(ci->rows, gridCols, 1);
+    // dim3 block(1, threadsPerBlock, blockZSize);
+
+    // printf("Grid: {%d, %d, %d} blocks. Blocks: ${%d, %d, %d} threads.\n", grid.x, grid.y, grid.z, block.x, block.y, block.z);
 
     // Kernel code
-    applyLightDarkGrayscale<<<grid, block>>>(ci->d_r, ci->d_g, ci->d_b, ci->d_bright, ci->d_dark, ci->d_grayscale, brightPercentage, darkPercentage);
+    applyLightDarkGrayscale<<<blocksPerGrid, threadsPerBlock>>>(ci->d_r, ci->d_g, ci->d_b, ci->d_bright, ci->d_dark, ci->d_grayscale, brightPercentage, darkPercentage);
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess) {
         fprintf(stderr, "Failed to launch bright kernel: %s\n", cudaGetErrorString(err));
@@ -64,14 +66,29 @@ __host__ int promptInputPercentage(string prompt) {
 }
 
 __host__ int main(int argc, char **argv) {
-    const int threadsPerBlock = 8;
+    // const int threadsPerBlock = 8;
+    dim3 threadsPerBlock;
+    dim3 blocksPerGrid;
+
+    threadsPerBlock.x = 32;
+    threadsPerBlock.y = 1;
+    threadsPerBlock.z = 1;
+
+    blocksPerGrid.x = 8;
+    blocksPerGrid.y = 1;
+    blocksPerGrid.z = 1;
+
     vector<string> filepaths;
     vector<CudaImage *> cudaImages;
 
     // Get a list of filenames
     string filepath = "./data";
     for (const auto &entry : filesystem::directory_iterator(filepath)) {
-        filepaths.push_back(entry.path());
+        string path = entry.path();
+        if (path.rfind(".gitkeep") == string::npos) {
+            filepaths.push_back(path);
+            cout << "Filepath added: " << path << "\n";
+        }
     }
 
     try {
@@ -81,28 +98,43 @@ __host__ int main(int argc, char **argv) {
         // Prompt user for percentage to darken images by
         int darkPercentage = promptInputPercentage("Enter percentage to darken second image");
 
+        cout << "Before start loop\n";
+
         // Iterate through each file
         CudaImage *ci = NULL;
         for (string path : filepaths) {
+            cout << "Size: " << cudaImages.size() << "\n";
+            cout << "Path: " << path << "\n";
             ci = createCudaImage(path);
             copyFromHostToDevice(ci);
 
+            cout << "Inside loop #1\n";
+
             // Execute kernel
-            executeKernel(ci, brightPercentage, darkPercentage, threadsPerBlock);
+            executeKernel(ci, brightPercentage, darkPercentage, threadsPerBlock, blocksPerGrid);
+
+            cout << "Inside loop #2\n";
 
             cudaImages.push_back(ci);
+
+            cout << "Inside loop #3\n";
         }
 
-        // Synchronize all threads
-        // __syncthreads();
+        // cout << "Before end loop\n";
+        cudaDeviceSynchronize();
 
         // Now that our data operations are finished, commence with mapping to output files
         for (int i = 0; i < filepaths.size(); ++i) {
+            cout << "In end loop #1\n";
             copyFromDeviceToHost(cudaImages[i]);
+
+            cout << "In end loop #2\n";
 
             mapBrightImage(cudaImages[i], "bright_" + filepaths[i]);
             mapDarkImage(cudaImages[i], "dark_" + filepaths[i]);
             mapGrayscaleImage(cudaImages[i], "grayscale_" + filepaths[i]);
+
+            cout << "Hello\n";
 
             destroyCudaImage(cudaImages[i]);
         }
